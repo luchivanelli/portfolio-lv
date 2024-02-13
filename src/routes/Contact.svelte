c990dbafdf78ec1ca2111ffc3b1b5a0d507ad007
Luciana <lucianavanelli13gmail.com>
2024-02-13 17:08:19 -0300

    commit

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,7 +1,7 @@
 <section class="bg-gradient-to-b from-red-300 to-red-700 flex flex-col items-center gap-2">
     <div class="relative m-4 lg:m-10">
-        <h3 class="text-5xl lg:text-6xl text-center text-red-600 font-extrabold tracking-wider opacity-40">Experience</h3>
-        <p class="title font-semibold text-3xl lg:text-4xl opacity-100 tracking-wider absolute top-1/2 bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Experience</p>
+        <h3 class="text-5xl lg:text-6xl text-center text-red-600 font-extrabold tracking-wider opacity-40">Contact</h3>
+        <p class="title font-semibold text-3xl lg:text-4xl opacity-100 tracking-wider absolute top-1/2 bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Contact</p>
     </div>
 
 </section>
